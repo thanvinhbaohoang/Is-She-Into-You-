17b4231a9865d0c500c7f5f57d9177c4ae226da1
Harold <vinh.bao.hoang.than.23@dartmouth.edu>
2022-04-13 23:34:57 -0400

    What the heck is going on here

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// change require to es6 import style
+// import $ from 'jquery';
+import $ from 'jquery';
+
+require('./styles.scss');
+require('./index.html');
+// import './style.scss';
+
+$('#main').html('Here we go!');
+
+// THIS IS SUPPOSED TO BE THE TIMER
+// let seconds = 0;
+// const element = document.getElementById('main');
+
+// function incrementSeconds() {
+//   seconds += 1;
+//   element.innerText = seconds;
+// }
+
+// setInterval(incrementSeconds, 1000);
